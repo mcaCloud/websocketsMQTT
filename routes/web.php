aa0329ecc19e601be36177915fb10d8b4382965d
mcespede <cespedes.mauro@gmail.com>
2020-05-27 16:08:57 +0200

    Las rutas de password reset tenian problemas y no conicidian con  los nombres . Ya estan corregidas y funcionando

--- routes/web.php
+++ routes/web.php
@@ -12,8 +12,7 @@ Route::get('/login', 'Auth\LoginController@showLoginForm')->name('login.form');
 
 Route::post('/login', 'Auth\LoginController@login')->name('login');
 
-
-Route::get('/logout', 'Auth\LoginController@logout')->name('logout');
+Route::get('auth/logout', 'Auth\LoginController@logout')->name('logout');
 /*************** /LOGIN-LOGOUT /******************************/
 /*
 /*
@@ -28,11 +27,12 @@ Route::get('/', 'Dashboard\HomeController@index')->name('index');
 */
 /*************** RESET-PASSWORD ******************************/
 /**/
+
 /******   1  *****/
-/*Esta es la ruta cuando el USER hace click en el 'Forgot Password', me lleva a la vista 'email'*/
+/*Esta es la ruta cuando el USER hace click en el 'Forgot Password', me lleva a la vista 'email' para que e ususario intoduzca el email*/
 /*Dentro el controlador 'ForgotPasswordController' solo hay una referencia hacia el TRAIT de 'SendsPasswordresetEmails'*/
 /*Dentro de ese TRAIT utilizo el metodo de 'showLinkRequestForm'*/
-Route::get('/password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.reset');
+Route::get('/password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
 
 /******   2   *****/
 /*Una vez dentro de la vista 'email' se envia el POST del formulario de reset password*/
@@ -40,20 +40,18 @@ Route::get('/password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm
 /*Dentro de ese TRAIT utilizo el metodo de 'sendResetLinkEmail'*/
 Route::post('/password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
 
-
 /******   3   *****/
+/*Cunado el user hace click en el link que recibe, lo envia a la vista para cambiar el password*/
 /* Dentro del controlador de 'ResetPasswordController' solo hace referencia a el TRAIT de 'ResetPasswords' ubicado en'Illuminate/Foundation/Auth/ResetPasswords'*/
 /*Dentro de ese TRAIT llamo al metodo 'showResetForm'*/
-Route::get('/password/reset/{token} ', 'Auth\ResetPasswordController@showResetForm')->name('password.change');
-
+Route::get('/password/reset/{token} ', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
 
 /******   4   *****/
+/*Finalmente se envia el request con un POST para cambiar el password*/
 /* Dentro del controlador de 'ResetPasswordController' solo hace referencia a el TRAIT de 'ResetPasswords' ubicado en'Illuminate/Foundation/Auth/ResetPasswords'*/
 /*Dentro de ese TRAIT llamo al metodo 'reset'*/
-Route::post('/password/reset', 'Auth\ResetPasswordController@reset')->name('password.request');
+Route::post('/password/reset', 'Auth\ResetPasswordController@reset')->name('password.update');
 /**********/
-
-/**/
 
 /*************** RESET-PASSWORD ******************************/
 /*
@@ -63,23 +61,33 @@ Route::post('/password/reset', 'Auth\ResetPasswordController@reset')->name('pass
 
 Route::group(['prefix' => 'dashboard', 'namespace' => 'Dashboard', 'as' => 'dashboard::'], function () {
 
+ /*Todas estas rutas estan agrupadas con el middleware 'auth' que es un metodo dentro del controllador 'router.php' ubicado en'Illuminate/Routing*/
+ /*aqui se pueden ver las diferentes rutas que el el metodo cubre*/
 
   Route::group(['middleware' => ['auth']], function () {
 
-    Route::get('/', 'HomeController@index')->name('index');
+        Route::get('/', 'HomeController@index')->name('index');
 
-    Route::resource('/users', 'UserController', ['except' => ['show']]);
-    Route::get('/users/{user}/access', 'UserController@toggleAccess')->name('users.toggleAccess');
-    Route::get('/users/export', 'UserController@export')->name('users.export');
+        Route::resource('/users', 'UserController', ['except' => ['show']]);
 
-    Route::resource('/roles', 'RoleController', ['except' => ['show']]);
-    Route::resource('/permissions', 'PermissionController', ['except' => ['show']]);
-    Route::get('/common/slug/{name}', 'CommonController@slug')->name('common.slug');
+        Route::get('/users/{user}/access', 'UserController@toggleAccess')->name('users.toggleAccess');
 
-    Route::get('/pdfs', 'PdfController@index')->name('pdfs.index');
-    Route::post('/pdfs/generate', 'PdfController@export_document_generate')->name('pdfs.export_document_generate');
-    Route::post('/pdfs/url', 'PdfController@export_document_url')->name('pdfs.export_document_url');
-    Route::get('/pdfs/view', 'PdfController@export_by_view')->name('pdfs.export_by_view');
+        Route::get('/users/export', 'UserController@export')->name('users.export');
+
+        Route::resource('/roles', 'RoleController', ['except' => ['show']]);
+
+
+        Route::resource('/permissions', 'PermissionController', ['except' => ['show']]);
+
+        Route::get('/common/slug/{name}', 'CommonController@slug')->name('common.slug');
+
+        Route::get('/pdfs', 'PdfController@index')->name('pdfs.index');
+
+        Route::post('/pdfs/generate', 'PdfController@export_document_generate')->name('pdfs.export_document_generate');
+
+        Route::post('/pdfs/url', 'PdfController@export_document_url')->name('pdfs.export_document_url');
+
+        Route::get('/pdfs/view', 'PdfController@export_by_view')->name('pdfs.export_by_view');
 
 
   });
